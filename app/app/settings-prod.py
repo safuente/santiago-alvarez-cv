49dc261eafd23a2df6eaa43038a37b684ecc4652
safuente <santiago.alvarezfuente@telefonica.com>
2021-06-14 23:59:26 +0200

    ngnix conf name 5

--- app/app/settings-prod.py
+++ app/app/settings-prod.py
@@ -125,11 +125,11 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 print(STATIC_ROOT)
 STATIC_URL = '/static/'
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
